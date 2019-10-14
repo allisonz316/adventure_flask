dd319cc568672c185fca43e6666aea83603f1359
Allison Z <alzig@udel.edu>
2019-10-14 16:17:03 -0400

    In the midst of attempting to establish battle system

--- views.py
+++ views.py
@@ -1,13 +1,17 @@
 from route_helper import simple_route
+import random
 
+character = {"name": "Lonk", "total_hp": 20, "current_hp": "20", "def": 8, "exp": 0, "lvl": 1}
 inventory = ["Rope", "Lantern", "Potion"]
 weapons = "Sword"
 
 GAME_HEADER = """
 <h1>Legend of Zolda: Parody of Time</h1>
 Your Inventory: {item1}, {item2}, {item3}<br>
-Your Weapon: {weapon}<p>
-""".format(item1=inventory[0], item2=inventory[1], item3=inventory[2], weapon=weapons)
+Your Weapon: {weapon}<br>
+Your HP: {current_hp}/{total_hp}<p>
+""".format(item1=inventory[0], item2=inventory[1], item3=inventory[2], weapon=weapons,
+           current_hp=character["current_hp"], total_hp=character["total_hp"])
 
 
 @simple_route('/')
@@ -41,13 +45,13 @@ Upon entering the forest, you see two paths in front of you.<p>
     
 Where will you go?<p>
     
-<a href="/goto/left/">Left path</a><br>
-<a href="/goto/right/">Right path</a>
+<a href="/generate/monster/left/">Left path</a><br>
+<a href="/generate/monster/right/">Right path</a>
     
 """
 
 GONE_LEFT = """
-You have gone left<p>
+Once you head down the left path, you come across a monster.<p>
 <a href='/'>Return to the start</a>
 """
 
@@ -81,3 +85,77 @@ def open_door(world: dict, where: str) -> str:
         return GAME_HEADER+GONE_LEFT
     elif where == "right":
         return GAME_HEADER+GONE_RIGHT
+
+
+@simple_route("/attack/")
+def attack(world: dict, current_monster: dict):
+    damage = 0
+    if weapons == "sword":
+        damage = random.randrange(4, 7)
+        damage = damage - (current_monster["def"]/2)
+    return damage
+
+
+@simple_route("/heal/")
+def heal(world: dict):
+    if "Potion" in inventory and character["current_hp"] < character["total_hp"]:
+        character["current_hp"] = character["current_hp"] + 10
+        inventory.remove(inventory[2])
+        return GAME_HEADER + """Healed 10 HP!
+                <a href="/goto/forest/">Enter the forest</a><br>
+        """
+    else:
+        return GAME_HEADER + """Couldn't heal!
+                <a href="/goto/forest/">Enter the forest</a><br>
+        """
+
+
+@simple_route("/flee/")
+def flee(world: dict):
+    flee_chance = random.randrange(1, 11)
+    if flee_chance >= 7:
+        return GAME_HEADER + """Successfully fled!<br>
+                <a href="/goto/forest/">Enter the forest</a><br>
+        """
+    else:
+        return GAME_HEADER + """Couldn't flee!
+                <a href="/goto/forest/">Enter the forest</a><br>
+        """
+
+
+@simple_route("/generate/monster/<where>/")
+def generate_monster(world: dict, where: str) -> str:
+    monster_number = random.randrange(1, 10)
+    current_monster = {}
+    if monster_number == 1:
+        current_monster = {"name": "Gel-O", "hp": 7, "atk": 4, "def": 1, "exp_drop": 3}
+    elif monster_number == 2:
+        current_monster = {"name": "Beehat", "hp": 12, "atk": 6, "def": 3, "exp_drop": 5}
+    elif monster_number == 3:
+        current_monster = {"name": "Knocktorok", "hp": 15, "atk": 5, "def": 4, "exp_drop": 6}
+    elif monster_number == 4:
+        current_monster = {"name": "Beever", "hp": 8, "atk": 3, "def": 2, "exp_drop": 4}
+    elif monster_number == 6:
+        current_monster = {"name": "Wolfy", "hp": 18, "atk": 8, "def": 5, "exp_drop": 10}
+    elif monster_number == 7:
+        current_monster = {"name": "Dangdango", "hp": 17, "atk": 7, "def": 9, "exp_drop": 12}
+    elif monster_number == 8:
+        current_monster = {"name": "Key", "hp": 6, "atk": 3, "def": 1, "exp_drop": 2}
+    elif monster_number == 9:
+        current_monster = {"name": "Wizzo", "hp": 10, "atk": 7, "def": 3, "exp_drop": 8}
+    world['monster'] = current_monster
+    world['location'] = where
+    if where == "left":
+        return GAME_HEADER + """You have gone left and found a {name}.<p>
+                             What will you do?<p>
+                             <a href="/attack/">Attack</a><br>
+                             <a href="/heal/">Heal</a><br>
+                             <a href="/flee/">Flee</a><br>
+                             """.format(name=current_monster["name"])
+    elif where == "right":
+        return GAME_HEADER + """You have gone right and found a {name}.<p>
+                             What will you do?<p>
+                             <a href="/attack/">Attack</a><br>
+                             <a href="/heal/">Heal</a><br>
+                             <a href="/flee/">Flee</a><br>
+                             """.format(name=current_monster["name"])
